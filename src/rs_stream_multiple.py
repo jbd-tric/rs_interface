564e3bb1e26d0fccf9b5c4d60989b172675e948e
jbd-tric <jasond@tricrobotics.com>
2024-10-11 13:28:07 -0700

    updates

--- src/rs_stream_multiple.py
+++ src/rs_stream_multiple.py
@@ -1,33 +1,78 @@
 import cv2
 import numpy as np
 import pyrealsense2 as rs
-import time, os
+import time, os, re, subprocess
 
 # TODO
-# 1. find all real sense cameras
+# 1. find all real sense cameras - DONE
 # 2. Index them by serial number
 
-captures = []
+# Constants
+RESOLUTION_WIDTH = 1280
+RESOLUTION_HEIGHT = 720
+FRAME_RATE = 30 # Max supported frame rate is 30 FPS
+# DISPLAY_FRAMES = False
+# RECORD_DURATION = 5 # seconds
 
-# captures.append(cv2.VideoCapture(3))
-# captures.append(cv2.VideoCapture(4))
-
-cap1 = cv2.VideoCapture(0)
-cap2 = cv2.VideoCapture(1)
-
-while True:
-    ret1, frame1 = cap1.read()
-    ret2, frame2 = cap2.read()
+def find_rs_devices(target_model=""):
     
-    if ret1:
-        cv2.imshow('Camera 1', frame1)
+    rs_devices = []
 
-    if ret2:
-        cv2.imshow('Camera 2', frame2)
+    ctx = rs.context()
+    for device in ctx.query_devices():
+        device.group_dict()
+        rs_devices.append(device)
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+    return rs_devices
 
-cap1.release()
-cap2.release()
-cv2.destroyAllWindows()
+def init_rs_pipeline(rs_devices):
+    
+    pipelines = []
+
+    for device in rs_devices:
+
+        pipeline = rs.pipeline()
+        config = rs.config()
+
+        config.enable_stream(rs.stream.depth, RESOLUTION_WIDTH, RESOLUTION_HEIGHT,rs.format.z16, FRAME_RATE)
+        pipeline.start(config)
+        pipelines.append((device['id'], pipeline))
+
+    return pipelines
+
+if __name__ == '__main__':
+    
+    rs_devices = find_rs_devices()              # find rs cameras on the bus
+    pipelines = init_rs_pipeline(rs_devices)    # start a separate pipeline for each camera
+
+    quit = False
+
+    while not quit:
+
+        for pipeline in pipelines:
+
+            frame = pipeline[1].poll_for_frames()
+
+            depth_frame = frame.get_depth_frame()
+
+            if not depth_frame:
+                continue
+
+            depth_image = np.asanyarray(depth_frame.get_data())
+            depth_image = cv2.convertScaleAbs(depth_image, alpha=0.03)
+            cv2.imshow(pipeline[0], depth_image)
+
+
+        # for c in captures:
+        #     ret, frame = c.read()
+
+        #     if ret:
+        #         cv2.imshow(device['id'], frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            quit = True
+            break
+
+    
+
+    cv2.destroyAllWindows()
